a6b532eb71dc4457b986809f0e9bf979c236e892
Oliver Siegemund <os@ventose.cc>
2021-11-30 01:06:34 +0100

    Implement basic serialization

--- datastore/ds-lib/src/lib.rs
+++ datastore/ds-lib/src/lib.rs
@@ -1,29 +1,98 @@
+extern crate flexbuffers;
 extern crate serde;
 extern crate serde_derive;
-use serde_derive::{Serialize, Deserialize};
+
+use crate::StoreSerializationError::{DeserializeError, ReaderError, SerializeError};
+use serde::{Deserialize, Serialize};
+use serde_derive::{Deserialize, Serialize};
 use std::collections::hash_map::HashMap;
 use std::path::Path;
 use std::path::PathBuf;
 
-
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Store {
-    pub index: HashMap<String, u64>,
-    pub data: Vec<u8>
+    index: HashMap<String, usize>,
+    meta: Vec<DataOption>,
+    data: Vec<Vec<u8>>,
+    know_index: Vec<usize>,
 }
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct DataOption {}
 
 pub struct Datastore {
     base_dir: PathBuf,
-    known_stores: Vec<String>
+    known_stores: Vec<String>,
 }
+
+pub enum StoreSerializationError {
+    DeserializeError,
+    SerializeError,
+    ReaderError,
+}
+
+impl Store {
+    pub fn new() -> Store {
+        Store {
+            index: HashMap::new(),
+            meta: Vec::new(),
+            data: Vec::new(),
+            know_index: Vec::new(),
+        }
+    }
+    fn new_index(&self) -> usize {
+        match self.know_index.last() {
+            Some(last) => return last + 1,
+            None => 1,
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        match self.index.is_empty() {
+            false => self.data.len(),
+            true => 0,
+        }
+    }
+
+    pub fn load(data: Vec<u8>) -> Result<Store, StoreSerializationError> {
+        let reader = match flexbuffers::Reader::get_root(data.as_slice()) {
+            Ok(reader) => reader,
+            Err(_err) => return Err(ReaderError),
+        };
+
+        let store = match Store::deserialize(reader) {
+            Ok(s) => s,
+            Err(_err) => return Err(DeserializeError),
+        };
+        Ok(store)
+    }
+
+    pub fn as_slice(&self) -> Result<Vec<u8>, StoreSerializationError> {
+        let mut s = flexbuffers::FlexbufferSerializer::new();
+        match self.serialize(&mut s) {
+            Ok(_) => Ok(s.take_buffer()),
+            Err(_) => Err(SerializeError),
+        }
+    }
+
+    pub fn add(&mut self, data: Vec<u8>) -> usize {
+        let inx = self.new_index();
+        self.index.insert("id=".to_string(), inx);
+        self.data.push(data);
+        self.meta.push(DataOption {});
+        self.know_index.push(inx);
+        inx
+    }
+}
+
 impl Datastore {
     pub fn new(path: PathBuf) -> Datastore {
-        Datastore{
+        Datastore {
             base_dir: path,
-            known_stores: Vec::new()
+            known_stores: Vec::new(),
         }
     }
     pub fn add_store(&mut self, name: String) {
-        self.known_stores.push(name)        
+        self.known_stores.push(name)
     }
 }
--- datastore/ds-lib/tests/bin-data-store.rs
+++ datastore/ds-lib/tests/bin-data-store.rs
@@ -1,25 +1,42 @@
-extern crate serde_json;
 extern crate flexbuffers;
+extern crate serde_json;
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
-    use std::ops::Index;
-    use serde::{Serialize, Deserialize};
     use super::*;
-    use serde_json::{Result, Value};
     use dslib::Store;
+    use serde::Serialize;
+    use serde_json::Value;
+
+    #[test]
+    fn test_store_add() {
+        let mut s = Store::new();
+        assert!(s.len() == 0);
+        let data: Vec<u8> = vec![1, 2];
+        s.add(data);
+        assert!(s.len() == 1);
+    }
+
+    #[test]
+    fn test_store_save_and_load() {
+        let mut s = Store::new();
+        s.add(vec![1, 2]);
+        s.add(vec![1, 2]);
+        let s_res = s.as_slice();
+        assert!(s_res.is_ok());
+        let s_vec = s_res.unwrap_or(Vec::new());
+        let l_res = Store::load(s_vec);
+        assert!(l_res.is_ok());
+        let l_s = l_res.unwrap_or(Store::new());
+        assert!(l_s.len() == 2);
+    }
 
     #[test]
     fn test_store_to_bin_and_back() {
-        let mut sp = Store {
-            data: vec![1, 0, 1, 0],
-            index: HashMap::new()
-        };
-        sp.index.insert("foo".to_string(), 1);
+        let sp = Store::new();
 
         let mut s = flexbuffers::FlexbufferSerializer::new();
-        if let Ok(_) = sp.serialize(&mut s){
+        if let Ok(_) = sp.serialize(&mut s) {
             let b = s.take_buffer();
             assert!(!b.is_empty());
         } else {
@@ -36,7 +53,7 @@ mod tests {
 
         let bytes = hg_hu.as_bytes();
 
-        let v:Value = serde_json::from_slice(bytes).unwrap();
+        let v: Value = serde_json::from_slice(bytes).unwrap();
         let v_obj = v.get("value").unwrap();
         let v_f64 = v_obj.as_f64().unwrap();
         assert!(v_f64 < 0.0);
@@ -45,13 +62,12 @@ mod tests {
 
     #[test]
     fn test_str_handling() {
-
         let json_str = r#"{
             "name": 12,
             "value": 1234E-10
             }"#;
 
-        let v:Value = serde_json::from_str(json_str).unwrap();
+        let v: Value = serde_json::from_str(json_str).unwrap();
         let j_obj = v.get("value").unwrap();
         let j_dou = j_obj.as_f64().unwrap();
         assert!(j_dou < 1e-6);
