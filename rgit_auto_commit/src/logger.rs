35bdd4b96bf422cd944431287886abd8b4d80b9c
Oliver Siegemund <47053492+Komplementariteten@users.noreply.github.com>
2023-10-14 18:51:52 +0200

    Auto commit @14-10-2023 18:51

--- rgit_auto_commit/src/logger.rs
+++ rgit_auto_commit/src/logger.rs
@@ -1,9 +1,9 @@
-use log::LevelFilter;
+use log::{info, LevelFilter};
 use systemd_journal_logger::{connected_to_journal, JournalLog};
 
 pub(crate) fn init_logger() {
-    // if connected_to_journal() {
-        JournalLog::new().expect("can't init journaled logger even when found").install().expect("Can't connect to journaled")
+    JournalLog::new().expect("can't init journaled logger even when found").install().expect("Can't connect to journaled");
     // }
-    // log::set_max_level(LevelFilter::Info);
+    log::set_max_level(LevelFilter::Info);
+    info!(target: "_TEST_LOG_TARGET", "connected_to_journal -> {}", connected_to_journal());
 }
