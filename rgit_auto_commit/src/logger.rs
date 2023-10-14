5aa3cfb8f7bd6777f8025b309b7dab7c8fdcfe55
Oliver Siegemund <47053492+Komplementariteten@users.noreply.github.com>
2023-10-14 18:52:43 +0200

    Auto commit @14-10-2023 18:52

--- rgit_auto_commit/src/logger.rs
+++ rgit_auto_commit/src/logger.rs
@@ -5,5 +5,4 @@ pub(crate) fn init_logger() {
     JournalLog::new().expect("can't init journaled logger even when found").install().expect("Can't connect to journaled");
     // }
     log::set_max_level(LevelFilter::Info);
-    info!(target: "_TEST_LOG_TARGET", "connected_to_journal -> {}", connected_to_journal());
 }
