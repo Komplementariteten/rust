f5535ace58759b541450c40808560e88e4256dd0
Komplementariteten <47053492+Komplementariteten@users.norepley.github.com>
2024-08-03 10:26:42 +0200

    Zeug

--- rs-sock/rs-rpc/src/rpc.rs
+++ rs-sock/rs-rpc/src/rpc.rs
@@ -1,11 +1,43 @@
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use serde::{Deserialize, Serialize};
 use serde::de::DeserializeOwned;
 
-pub trait RpcParameter: Serialize + DeserializeOwned + Sized  + Send {
-}
+pub trait RpcParameter: Sized + Serialize + DeserializeOwned + Clone {}
 
-pub trait RpcResult: Serialize + DeserializeOwned + Sized + Sync {}
+pub trait RpcResult: Sized + Serialize + DeserializeOwned + Clone {}
 
 pub trait RpcFnc {
-    // fn call(&self, parameter: dyn RpcParameter + Send + Sync) -> dyn RpcResult;
+    fn call(&self, parameter: impl RpcParameter) -> impl RpcResult;
 }
+
+impl RpcResult for Vec<i32> {}
+
+impl RpcResult for Vec<f32> {}
+
+impl RpcResult for String {}
+
+impl RpcResult for Vec<i64> {}
+
+#[cfg(test)]
+struct TestRpcFnc {}
+
+#[cfg(test)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct TestRpcParameter {}
+#[cfg(test)]
+impl RpcParameter for TestRpcParameter {}
+
+#[cfg(test)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct TestRpcResult {}
+#[cfg(test)]
+impl RpcResult for TestRpcResult {}
+
+#[cfg(test)]
+impl RpcFnc for TestRpcFnc {
+    fn call(self, parameter: TestRpcParameter) -> TestRpcResult {
+        todo!()
+    }
+}
+
+#[test]
+fn test_serialize_deserialze() {}
