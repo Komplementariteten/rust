083155a88b1202fa0c8b197b5bec899f198d7a99
Komplementariteten <47053492+Komplementariteten@users.noreply.github.com>
2025-02-19 20:03:24 +0100

    Auto commit @19-02-2025 20:03

---
+++ gtk-demo/src/main.rs
@@ -0,0 +1,15 @@
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow};
+
+const APP_ID: &str = "org.gtk_rs.HelloWorld2";
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+    app.connect_activate(build_ui);
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    let window = ApplicationWindow::builder().application(app).title("Hallo Welt").build();
+    window.present();
+}
